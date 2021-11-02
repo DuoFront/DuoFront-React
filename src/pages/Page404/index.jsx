db26f95ffd5bcc85e54a33b326a92586ae60685c
PaolaGuedes <paolaguedesbacca2002@gmail.com>
2021-11-02 13:43:59 -0300

    page de erro

--- src/pages/Page404/index.jsx
+++ src/pages/Page404/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import erro from '../../assets/img/404.png'
+import './Page404.scss'
 
 export function Page404(){
   return(
-    <h1>Ops, essa pagina não existe</h1>
+    <div className="main">
+    <h1 className="main__text">Oops, essa página não existe</h1>
+    <img className="main__img" src={erro}/>
+    </div>
   )
 }
