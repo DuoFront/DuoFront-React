5ab0808a04c8f31960b0727bd1123758199df2b5
alexRicc2 <alex.65.santanna@gmail.com>
2021-11-03 00:14:47 -0300

    ajustes do login

--- src/Components/Header/Navigation.jsx
+++ src/Components/Header/Navigation.jsx
@@ -1,61 +1,59 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { useAuth } from "../../hooks/Auth"
 import { Link } from "react-router-dom"
-import '../../pages/LoginPage/index'
 
 const listaMenu = [
-  {id:1, link: 'https://www.google.com', text: 'Layouts'},
-  {id:2, link: '../../pages/LoginPage/index', text: 'Entrar'},
-  {id:3, link: 'https://www.google.com', text: 'Cadastrar'},
+  { id: 1, link: '#', text: 'Layouts' },
+  { id: 3, link: '#', text: 'Cadastrar' },
 ]
 
-export function Navigation({lista = listaMenu}){
+export function Navigation({ lista = listaMenu }) {
 
   let iconOpen = faBars
   const [showMenu, setShowMenu] = useState(false)
-  const {user, signOut, signUrl} = useAuth()
+  const { user, signOut } = useAuth()
 
   let menu
 
-  if(showMenu){
+  if (showMenu) {
     iconOpen = faTimes
-    menu =  
-    (<ul className="header__menu">
-    {
-      lista.map((item) => (
-        <li key={item.id}>
-          <Link
-          className="header__link"
-          to={item.link}>
-            {item.text}
-            </Link>
-        </li>
-      ))
-    }
-    {user &&
-    <>
-      <li onClick={signOut} className="header__link">Logout</li>
-      <img src={user.avatar_url} alt={user.name} className="avatar"/>
-      <li>{user.name}</li>
-    </>
-    }
-    {!user &&
-    
-      <li className="header__link"><a href={signUrl}>Entrar</a></li>
-    }
-  </ul>)
+    menu =
+      (<ul className="header__menu">
+        {
+          lista.map((item) => (
+            <li key={item.id}  className="header__link">
+              <Link
+               
+                to={item.link}>
+                {item.text}
+              </Link>
+            </li>
+          ))
+        }
+        {user &&
+          <>
+            <li onClick={signOut} className="header__link">Logout</li>
+            <img src={user.avatar_url} alt={user.name} className="avatar" />
+            <li>{user.name}</li>
+          </>
+        }
+        {!user &&
+
+          <li className="header__link"><Link to="/login">Entrar</Link></li>
+        }
+      </ul>)
   }
-  
-  return(
+
+  return (
     <nav>
-    <FontAwesomeIcon 
-    icon={iconOpen}
-    className="header__icon"
-    onClick={ () => (setShowMenu(!showMenu))}
-    />
-    { menu }
-  </nav>
+      <FontAwesomeIcon
+        icon={iconOpen}
+        className="header__icon"
+        onClick={() => (setShowMenu(!showMenu))}
+      />
+      {menu}
+    </nav>
   )
 }
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,17 +1,24 @@
 import React from "react"
 import './Login.scss'
 import { ButtonDefault } from '../ButtonDefault/ButtonDefault'
+import { Link } from "react-router-dom"
+import { useAuth } from "../../hooks/Auth"
 
 export function Login() {
+
+  const {signUrl} = useAuth();
+
   return (
     <main className="mainLogin">
-      <h1 className="mainLogin__logo">DuoFront</h1>
+      <h1 className="mainLogin__logo"><Link to="/">DuoFront</Link></h1>
       <h2 className="mainLogin__title">
           Pronto para iniciar sua jornada?
       </h2>
       <ButtonDefault>
+        <a href={signUrl}>
       <ion-icon name="logo-github"></ion-icon>
           <span className="btn__text">Entrar com github</span>
+        </a>
       </ButtonDefault>
     </main>
   )
